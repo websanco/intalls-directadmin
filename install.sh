6722f029175f2b0eba401df659f5e4e23635a1c6
websanco <32726791+websanco@users.noreply.github.com>
2017-10-12 22:29:03 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,4 +10,7 @@
 # past this point for scp and rcp, and it's important to refrain from
 # outputting anything in those cases.
 
-
+yum install wget gcc gcc-c++ flex bison make bind bind-libs bind-utils openssl openssl-devel perl quota libaio libcom_err-devel libcurl-devel gd zlib-devel zip unzip libcap-devel cronie bzip2 cyrus-sasl-devel perl-ExtUtils-Embed autoconf automake libtool which patch mailx db4-devel  
+wget http://www.directadmin.com/setup.sh  
+chmod 755 setup.sh  
+./setup.sh
