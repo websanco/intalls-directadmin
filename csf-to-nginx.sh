8ad063300a60dcf1ab561b26ebeb30164d37f309
websanco <32726791+websanco@users.noreply.github.com>
2017-10-16 10:18:51 +0700

    Update csf-to-nginx.sh

--- csf-to-nginx.sh
+++ csf-to-nginx.sh
@@ -18,7 +18,10 @@ service csf restart
 /sbin/iptables -I INPUT 1 -p tcp --dport 2222 -j ACCEPT  
 /sbin/iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT  
 /sbin/iptables -I INPUT 1 -p tcp --dport 21 -j ACCEPT  
-/sbin/iptables -I INPUT 1 -p tcp --dport 8080 -j ACCEPT  
+/sbin/iptables -I INPUT 1 -p tcp --dport 8080 -j ACCEPT
+/sbin/iptables -I INPUT 1 -p tcp --dport 25 -j ACCEPT 
+/sbin/iptables -I INPUT 1 -p tcp --dport 465 -j ACCEPT
+/sbin/iptables -I INPUT 1 -p tcp --dport 587 -j ACCEPT  
 service iptables save  
 echo "letsencrypt=1" >> /usr/local/directadmin/conf/directadmin.conf  
 echo "enable_ssl_sni=1" >> /usr/local/directadmin/conf/directadmin.conf
