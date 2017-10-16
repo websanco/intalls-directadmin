0185403ca1e701f4aa792b3087074c376e5040db
websanco <32726791+websanco@users.noreply.github.com>
2017-10-16 10:34:17 +0700

    Update csf-to-nginx.sh

--- csf-to-nginx.sh
+++ csf-to-nginx.sh
@@ -11,28 +11,7 @@
 
 
 
-wget http://files.directadmin.com/services/all/csf/csf_install.sh  
-/bin/sh ./csf_install.sh
-chkconfig --level 235 csf on  
-service csf restart
-/sbin/iptables -I INPUT 1 -p tcp --dport 2222 -j ACCEPT  
-/sbin/iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT  
-/sbin/iptables -I INPUT 1 -p tcp --dport 21 -j ACCEPT  
-/sbin/iptables -I INPUT 1 -p tcp --dport 8080 -j ACCEPT
-/sbin/iptables -I INPUT 1 -p tcp --dport 25 -j ACCEPT 
-/sbin/iptables -I INPUT 1 -p tcp --dport 465 -j ACCEPT
-/sbin/iptables -I INPUT 1 -p tcp --dport 587 -j ACCEPT  
-service iptables save  
-echo "letsencrypt=1" >> /usr/local/directadmin/conf/directadmin.conf  
-echo "enable_ssl_sni=1" >> /usr/local/directadmin/conf/directadmin.conf
-echo "check_subdomain_owner=0" >> /usr/local/directadmin/conf/directadmin.conf  
-echo "hide_ip_user_numbers=0" >> /usr/local/directadmin/conf/directadmin.conf 
-/etc/init.d/directadmin restart  
-wget -O /usr/local/directadmin/scripts/letsencrypt.sh http://files.directadmin.com/services/all/letsencrypt.sh  
-cd /usr/local/directadmin/custombuild  
-./build update  
-./build letsencrypt  
-./build rewrite_confs 
+
 cd /usr/local/directadmin/custombuild
 ./build update
 ./build set php1_release 5.6
