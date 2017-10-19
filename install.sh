de2b07db7fd4a0cf5784fa312038f5d77162ec9b
websanco <32726791+websanco@users.noreply.github.com>
2017-10-19 13:29:03 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@
 # outputting anything in those cases.
 
 #intall
-yum install wget gcc gcc-c++ flex bison make bind bind-libs bind-utils openssl openssl-devel perl quota libaio libcom_err-devel libcurl-devel gd zlib-devel zip unzip libcap-devel cronie bzip2 cyrus-sasl-devel perl-ExtUtils-Embed autoconf automake libtool which patch mailx db4-devel
+yum install gcc gcc-c++ flex bison make bind bind-libs bind-utils openssl openssl-devel perl quota libaio libcom_err-devel libcurl-devel gd zlib-devel zip unzip libcap-devel cronie bzip2 cyrus-sasl-devel perl-ExtUtils-Embed autoconf automake libtool which patch mailx db4-devel
 wget http://www.directadmin.com/setup.sh  
 chmod 755 setup.sh  
 ./setup.sh
